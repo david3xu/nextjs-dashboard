a6245c36cede66e20175c6bd926d80186a3256c6
David <291928k@curtin.edu.au>
2024-08-25 22:30:38 +0800

    chapter 9

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,5 @@
+import DashboardSkeleton from "../../ui/skeletons"; // 👈 Import the DashboardSkeleton component: '@/app/ui/skeletons'
+
+export default function Loading() {
+  return <DashboardSkeleton />; // 👈 Render the DashboardSkeleton component
+}
